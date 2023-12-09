49248545bd5b001aea766f501907370da7985d2b
muhnasih <144122240+muhnasih@users.noreply.github.com>
2023-12-10 02:07:43 +0700

    Update DasproProjekkelompokJobsheet3.java

--- daspro-projek-kelompok/DasproProjekkelompokJobsheet3.java
+++ daspro-projek-kelompok/DasproProjekkelompokJobsheet3.java
@@ -12,7 +12,7 @@ public class DasproProjekkelompokJobsheet3 {
         String[] mataPelajaran = {"Daspro", "prakdaspro", "pkn", "matematika", "ctps", "k3", "bahasa inggris", "kti"};
 
         boolean continueLogin = true;
-        int[][] nilaiSiswa = new int[kelas.length][];
+        int[][][] nilaiSiswa = new int[kelas.length][3][];
         String[][] namaSiswa = new String[kelas.length][];
 
         while (continueLogin) {
@@ -46,23 +46,40 @@ public class DasproProjekkelompokJobsheet3 {
                                 int mhsw = sc.nextInt();
                                 sc.nextLine(); 
 
-                                nilaiSiswa[indexKelas] = new int[mhsw];
+                                nilaiSiswa[indexKelas][0] = new int[mhsw]; //nilai tugas
+                                nilaiSiswa[indexKelas][1] = new int[mhsw]; //nilai ujian
+                                nilaiSiswa[indexKelas][2] = new int[mhsw]; //nilai praktek
                                 namaSiswa[indexKelas] = new String[mhsw];
 
                                 for (int i = 0; i < mhsw; i++) {
                                     System.out.println("Masukkan nama mahasiswa " + (i + 1) + " : ");
                                     namaSiswa[indexKelas][i] = sc.nextLine();
-                                    int totalNilai = 0;
+                                    int totalNilaiTugas = 0;
+                                    int totalNilaiUjian = 0;
+                                    int totalNilaiPraktek = 0;
 
                                     for (int j = 0; j < mataPelajaran.length; j++) {
-                                        System.out.println("Masukkan nilai " + mataPelajaran[j] + " untuk " + namaSiswa[indexKelas][i] + " : ");
-                                        int nilai = sc.nextInt();
-                                        sc.nextLine();
-                                        totalNilai += nilai;
+                                        if (j < 2) { //tugas dan ujian
+                                            System.out.println("Masukkan nilai " + mataPelajaran[j] + " untuk " + namaSiswa[indexKelas][i] + " : ");
+                                            int nilai = sc.nextInt();
+                                            sc.nextLine();
+                                            if (j == 0) {
+                                                totalNilaiTugas += nilai;
+                                            } else {
+                                                totalNilaiUjian += nilai;
+                                            }
+                                        } else { //praktek
+                                            System.out.println("Masukkan nilai " + mataPelajaran[j] + " untuk " + namaSiswa[indexKelas][i] + " : ");
+                                            int nilai = sc.nextInt();
+                                            sc.nextLine();
+                                            totalNilaiPraktek += nilai;
+                                        }
                                     }
 
-                                
-                                    nilaiSiswa[indexKelas][i] = totalNilai;
+                                    // Simpan total nilai ke dalam array nilaiSiswa
+                                    nilaiSiswa[indexKelas][0][i] = totalNilaiTugas;
+                                    nilaiSiswa[indexKelas][1][i] = totalNilaiUjian;
+                                    nilaiSiswa[indexKelas][2][i] = totalNilaiPraktek;
                                 }
 
                                 System.out.println("Pengisian nilai selesai.");
@@ -71,18 +88,30 @@ public class DasproProjekkelompokJobsheet3 {
                             }
                             break;
 
+                        // ... (bagian kode sebelumnya)
+
                         case 2:
-                            for (int i = 0; i < kelas.length; i++) {
-                                System.out.println("Kelas " + kelas[i] + ":");
-                                if (nilaiSiswa[i] != null) {
-                                    for (int k = 0; k < nilaiSiswa[i].length; k++) {
-                                        System.out.println( namaSiswa[i][k] + " : " + calculateRataNilai(nilaiSiswa[i][k], mataPelajaran.length));
-                                    }
-                                } else {
-                                    System.out.println("Belum ada data nilai untuk kelas ini.");
+                        System.out.println("Nilai Setiap Kelas:");
+                        System.out.println("+-------+--------------+----------------+");
+                        System.out.println("| Kelas | Nama Siswa   | Rata-rata Nilai |");
+                        System.out.println("+-------+--------------+----------------+");
+                        for (int i = 0; i < kelas.length; i++) {
+                            if (nilaiSiswa[i][0] != null) {
+                                for (int k = 0; k < nilaiSiswa[i][0].length; k++) {
+                                    double rataTugas = calculateRataNilai(nilaiSiswa[i][0][k], 2);
+                                    double rataUjian = calculateRataNilai(nilaiSiswa[i][1][k], 2);
+                                    double rataPraktek = calculateRataNilai(nilaiSiswa[i][2][k], 1);
+                                    double rataNilai = (rataTugas + rataUjian + rataPraktek) / 3;
+                                    System.out.printf("| %-5s | %-12s | %-15.2f |\n", kelas[i], namaSiswa[i][k], rataNilai);
                                 }
+                            } else {
+                                System.out.println("| " + kelas[i] + " | Belum ada data nilai untuk kelas ini. | -              |");
                             }
-                            break;
+                            System.out.println("+-------+--------------+----------------+");
+                        }
+                        break;
+
+// ... (bagian kode setelahnya)
 
                         case 3:
                             System.out.println("Keluar dari program.");
@@ -106,7 +135,7 @@ public class DasproProjekkelompokJobsheet3 {
         sc.close();
     }
 
-         static int getIndexKelas(String kelasTerpilih) {
+    static int getIndexKelas(String kelasTerpilih) {
         // Mengonversi input 1a-1i menjadi indeks
         char kelasChar = kelasTerpilih.charAt(0);
         char subKelasChar = kelasTerpilih.charAt(1);
